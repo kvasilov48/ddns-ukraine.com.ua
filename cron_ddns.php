06fd9c2f0cd862528baf2eefd5abad1502b9239c
Aleksey <kvasilov48@users.noreply.github.com>
2015-04-13 21:23:11 +0300

    Update cron_ddns.php

--- cron_ddns.php
+++ cron_ddns.php
@@ -30,25 +30,12 @@ function ukr_response($fields){
 	return $info;
 }
 
-// Ф-ция получает айпишник машини на которой выполняется скрипт.
-function get_real_ip(){
-	$a = file_get_contents("http://internet.yandex.ru/");
-	$start = mb_strpos($a, "IPv4:")+6;
-	$a = mb_substr($a, $start);
-	$end = mb_strpos($a, " ");
-	$ip = mb_substr($a, 0, $end);
-	if(preg_match('/^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])'.'\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?|[0-9])$/', $ip) == FALSE){
-		die(date("Y-m-d H:i:s")." - Получил не правильный IP адресс -".$ip);
-	}
-	return trim($ip);
-}
-
 // Строка запроса для метода 'info'.
 $field = array('auth_login'=>$auth_login,'auth_token'=>$auth_token,'format'=>"xml",'class'=>"dns_record",'method'=>"info",'domain'=>$domain);
 $info = ukr_response($field);
 
 // Получаем реальний айпишник нашего сервера. Если ненужно просто установите новый IP в переменную $real_ip.
-$real_ip = get_real_ip();
+$real_ip = file_get_contents('http://curlmyip.com/');
 
 // Перебираем все записи, ищем наш поддомен
 for($i=0;$i<count($info);$i++){
